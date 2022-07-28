2b541ab72265ce29f64d6f15606019867baec973
Victor <edvictorsr@gmail.com>
2022-07-28 11:19:20 -0400

    estilos

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,60 +1,62 @@
 import styled, { keyframes, css } from "styled-components";
 
+
 export const Container = styled.div`
   max-height: 700px;
   margin: 80px auto;
   background: #fff;
   padding: 30px;
-  border-radius: 8px;
+  border-radius: 7px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-
   h1 {
+    display: flex;
     align-items: center;
     justify-content: center;
-    display: flex;
 
     svg {
-      margin-right: 5px;
+      margin-right: 10px;
     }
   }
 `;
 
 export const Form = styled.form`
-  margin: 20px auto;
+  margin-top: 30px;
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
   input {
-    border: solid 1px #0d2636;
     flex: 1;
+    border: ${({ error }) => (error ? `#FF0000` : `#222`)} solid 1px;
     padding: 7px 5px;
     border-top-left-radius: 7px;
     border-bottom-left-radius: 7px;
   }
 `;
 
-//animacao
+//ANIMACAO DO BOTAO
 const animated = keyframes`
-    from{
-        transform:rotate(0deg);
-    }
+  from{
+    transform:rotate(0deg);
+  }
 
-    to{
-        transform:rotate(360deg);
-    }
-
+  to{
+    transform:rotate(360deg);
+  }
 `;
 
 export const SubmitButton = styled.button.attrs(({ loading }) => ({
   type: "submit",
   disabled: loading,
 }))`
-  background: #0d2636;
-  border-top-right-radius: 7px;
-  border-bottom-right-radius: 7px;
-  padding: 7px;
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 7px 5px;
+  background: #0d2636;
+  border-top-right-radius: 7px;
+  border-bottom-right-radius: 7px;
 
   &[disabled] {
     opacity: 0.7;
@@ -70,31 +72,35 @@ export const SubmitButton = styled.button.attrs(({ loading }) => ({
     `}
 `;
 
+export const DeleteButton = styled.button.attrs({
+  type: `button`,
+})`
+  background: transparent;
+  padding: 8px 7px;
+  outline: none;
+  border: 0;
+  border-radius: 4px;
+`;
+
 export const List = styled.ul`
-  margin: 10px auto;
   list-style: none;
+  margin-top: 20px;
 
   li {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
     align-items: center;
-    padding: 5px;
-
-    & + li {
-      border-top: solid 1px #eee;
-    }
+    justify-content: space-between;
+    padding: 7px 5px;
+    color: #0d2636;
 
     span {
       display: flex;
       align-items: center;
-      svg {
-        margin-right: 10px;
-      }
+    }
+
+    & + li {
+      border-top: solid 1px #eee;
     }
   }
 `;
-
-export const DeleteButton = styled.button.attrs({
-  type: "button",
-})``;
